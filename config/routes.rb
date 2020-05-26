33ec493c7790e788201572d960ed7f0b8ac51723
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-05-26 12:44:16 -0300

    creating the get for finding a specific object and alias in the router, created the method in the controllerto find, linked the index to the page of the show.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get 'tasks/:id', to: 'tasks#show', as: 'task' # passing the id as a params so i can find the object by id.
   get 'tasks', to: 'tasks#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
